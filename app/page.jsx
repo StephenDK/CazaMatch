71150cb4b547e3350da2724156c0751fd4140681
StephenDK <stephendklein08@gmail.com>
2025-06-07 13:22:54 -0700

    File-Based Routing

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,24 @@
+// Importing Link to navigate to ather pages
+import Link from "next/link";
+
 const HomePage = ({ children }) => {
-  return <div className="text-2xl">HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">Go to Properties</Link>
+    </div>
+  );
 };
 
 export default HomePage;
 
-// By default, this component is being rendered on the server.
+// # page.jsx: By default, this component is being rendered on the server.
+// Since it is located in the Root app folder
+
+// # Link tag:
+// We can use Link in three ways. First way:
+// 1. <Link href="/properties">Go to Properties</Link>
+// 2. <Link href="/properties?name=test">Go to Properties</Link>
+// We can also pass a object to a link tag, for example:
+// 3. <Link href={{ pathname: 'properties', query: { name: 'test'} }}>Go To Properties</Link>
+// 2 and 3 will result in the URL being '/properties?name=test
---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,12 @@
+const PropertyPage = () => {
+  return <div>Property Page</div>;
+};
+
+export default PropertyPage;
+
+// This is an example of a dynamic route. For example,
+// This page will load when a user visits "properties/:id"
+// Take not of the folder created for this route "[id]"
+// You can change id to whatever we want.
+// *IMPORTANT* If we wanted to maje this route a catch all, for example, "properties/:id/rooms"
+// We would add three dots to the folder name "[...id]"
