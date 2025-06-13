1bb0236011a1b3581832b1dc6973db794a6bf6bd
StephenDK <stephendklein08@gmail.com>
2025-06-12 22:00:14 -0700

    Server actions

---
+++ app/actions/addProperty.js
@@ -0,0 +1,10 @@
+"use server";
+
+async function addProperty(formData) {
+  console.log("[ADD PROPERTY ACTION]");
+  // Getting data from the form.
+  // Server actions are added to the action filed in a html form element
+  //   console.log("[FORM DATA]: ", formData.get("name"));
+}
+
+export default addProperty;
