35133c8e5e998f4a88842e1af11b6f4d1b47d374
StephenDK <stephendklein08@gmail.com>
2025-06-14 22:39:09 -0700

    Display Messages

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -3,6 +3,7 @@ import Message from "@/models/Message";
 import "@/models/Property";
 import { convertToSerializedObject } from "@/utils/convertToObject";
 import { getSessionUser } from "@/utils/getSessionUser";
+import MessageCard from "@/components/MessageCard";
 
 const MessagesPage = async () => {
   await connectDB();
@@ -50,7 +51,7 @@ const MessagesPage = async () => {
               <p>You have no messages</p>
             ) : (
               messages.map((message, index) => (
-                <h3 key={message._id}>{message.name}</h3>
+                <MessageCard key={message._id} message={message} />
               ))
             )}
           </div>
