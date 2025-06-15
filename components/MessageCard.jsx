2c0f6e903f32ba86ba394c64572c91ae0925e8b7
StephenDK <stephendklein08@gmail.com>
2025-06-15 13:17:02 -0700

    Global Context For Message Count

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -2,9 +2,11 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import markMessageAsRead from "@/app/actions/markMessageAsRead";
+import deleteMessage from "@/app/actions/deleteMessage";
 
 const MessageCard = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
+  const [isDeleted, setIsDeleted] = useState(false);
 
   const handleReadClick = async () => {
     const read = await markMessageAsRead(message._id);
@@ -12,6 +14,16 @@ const MessageCard = ({ message }) => {
     setIsRead(read);
     toast.success(`Marked As ${read ? "Read" : "New"}`);
   };
+
+  const handleDeleteClick = async () => {
+    await deleteMessage(message._id);
+    setIsDeleted(true);
+    toast.success("Message Deleted");
+  };
+
+  if (isDeleted) {
+    return <p>Deleted Message</p>;
+  }
 
   return (
     <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
@@ -50,7 +62,10 @@ const MessageCard = ({ message }) => {
       >
         {isRead ? "Mark As New" : "Mark As Read"}
       </button>
-      <button className="mt-4 mr-3 bg-red-500 text-white py-1 px-3 rounded-md">
+      <button
+        className="mt-4 mr-3 bg-red-500 text-white py-1 px-3 rounded-md"
+        onClick={handleDeleteClick}
+      >
         Delete
       </button>
     </div>
